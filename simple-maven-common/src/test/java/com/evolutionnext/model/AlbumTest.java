0927813265387bb075c0107576b15c418f9c1565
Hao Leng <hao.leng@dish.com>
2015-09-30 09:53:32 -0600

    break test case

--- simple-maven-common/src/test/java/com/evolutionnext/model/AlbumTest.java
+++ simple-maven-common/src/test/java/com/evolutionnext/model/AlbumTest.java
@@ -14,5 +14,9 @@ public class AlbumTest {
         album.setId(13L);
         assertThat(album.getName()).isEqualTo("Rumours");
         assertThat(album.getId()).isEqualTo(13L);
+
+        //Below are changed made by Hao to break it
+        album.setId(15L);
+	assertThat(album.getId()).isEqualTo(17L);
     }
 }
